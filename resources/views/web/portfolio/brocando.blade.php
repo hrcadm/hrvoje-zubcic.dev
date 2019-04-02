0cef7ca46516937da861ba8264c1ebda8d92f0b5
hrcadm <hrcamnlz@gmail.com>
2019-04-02 13:37:38 +0200

    adding content to the projects

--- resources/views/web/portfolio/brocando.blade.php
+++ resources/views/web/portfolio/brocando.blade.php
@@ -15,9 +15,31 @@
     <section class="block" style="margin-bottom:5em;">
         <div class="container">
             <h3>Project details</h3>
-            <p></p>
+            <p><a href="https://brocando.ch" target="_blank">BroCanDo</a> is a Swiss based company offering health insurance services for citizens of Switzerland.</p>
+            <p>I was involved with this project while working at <a href="https://gauss.hr">Gauss Development Ltd</a> company.</p>
+            <p>Since I was the newcomer to the company at the time, I was given Sascha's (CEO of BroCanDo) web application project to debug and fix existing errors.</p>
+            <p>It seemed like an "easy fix" job, but soon enough it turned out to be faulty at its core functionality. The job was though, since the app was fully developed by several developers who were no longer present at the company and without any documentation (except some mobile devices API endpoints)
+                It took 2 months of hard work alongside my colleague Luka B. to get the app working properly again. But, we did it even before the deadline, which made Sascha very pleased with us and the company.</p>
+            <small>hint: formerly developed app crashed very soon, while this version still stands still!</small>
+            <p>BroCanDo had a massive impact in my programming career as it took all of my thinking resources to get it working again. I've learned so much about PHP, Symfony framework and many other things. So, I'll be always thankful to both the Gauss Development CEO Ivan Lozancic for hiring me in the first place and giving me Sascha's project to work on, and BroCanDo CEO Sascha Teorodovic for being flexible and supportive throughout the process. </p>
 
-            <h4>Tech Stack</h4>
+            <div>
+                <h4>Tech Stack</h4>
+                <span class="tech-stack-item">PHP</span>
+                <span class="tech-stack-item">Symfony</span>
+                <span class="tech-stack-item">HTML</span>
+                <span class="tech-stack-item">CSS</span>
+                <span class="tech-stack-item">MySQL</span>
+                <span class="tech-stack-item">Rest API</span>
+                <span class="tech-stack-item">LAMP</span>
+                <span class="tech-stack-item">Git</span>
+                <span class="tech-stack-item">Android/iOS</span>
+                <span class="tech-stack-item">UI/UX</span>
+            </div>
+
+            <div style="margin-top:3em;">
+                <a href="{{ URL::previous() }}" class="btn btn-dark">Back</a>
+            </div>
         </div>
     </section>
 @endsection
--- resources/views/web/portfolio/giftcash.blade.php
+++ resources/views/web/portfolio/giftcash.blade.php
@@ -15,9 +15,31 @@
     <section class="block" style="margin-bottom:5em;">
         <div class="container">
             <h3>Project details</h3>
-            <p></p>
+            <p>GiftCash Inc is a Canadian startup company, based in Toronto.</p>
+            <p>I've had a great opportunity to work with them, alongside Colin Moffat (the GiftCash CEO) and a well seasoned and experienced PHP developer <a href="https://johnshipp.com" target="_blank">John Shipp</a>.</p>
+            <p><a href="https://giftcash.com" target="_blank">GiftCash</a> is a service that offers you to redeem your gift cards for cash.</p>
+            <p>I was in charge for both backend and frontend tasks. Mainly optimizing backend services and speeding up the processes while implementing new logic. Also, creating new functional components and rendering dynamic statistics with Vue.js charts</p>
+            <p>It was a great experience working with GiftCash, where my ideas and actions were highly appreciated, praised and implemented.</p>
 
-            <h4>Tech Stack</h4>
+            <div>
+                <h4>Tech Stack</h4>
+                <span class="tech-stack-item">PHP</span>
+                <span class="tech-stack-item">Laravel</span>
+                <span class="tech-stack-item">JavaScript</span>
+                <span class="tech-stack-item">Vue.js</span>
+                <span class="tech-stack-item">Rest API</span>
+                <span class="tech-stack-item">JSON</span>
+                <span class="tech-stack-item">HTML</span>
+                <span class="tech-stack-item">CSS</span>
+                <span class="tech-stack-item">MySQL</span>
+                <span class="tech-stack-item">Rest</span>
+                <span class="tech-stack-item">LAMP</span>
+                <span class="tech-stack-item">Git</span>
+            </div>
+
+            <div style="margin-top:3em;">
+                <a href="{{ URL::previous() }}" class="btn btn-dark">Back</a>
+            </div>
         </div>
     </section>
 @endsection
